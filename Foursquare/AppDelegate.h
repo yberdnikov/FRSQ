75778d265cc3fb1344cbe727a6529a39d72c2b18
Yuriy <yberdnikov@me.com>
2015-06-09 09:39:24 +0300

    Initial Commit

---
+++ Foursquare/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Foursquare
+//
+//  Created by Yuriy Berdnikov on 6/9/15.
+//  Copyright (c) 2015 Yuriy Berdnikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Foursquare/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Foursquare
+//
+//  Created by Yuriy Berdnikov on 6/9/15.
+//  Copyright (c) 2015 Yuriy Berdnikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
